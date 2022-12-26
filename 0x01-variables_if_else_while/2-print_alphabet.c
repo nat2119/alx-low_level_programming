6cf7f7285fda6a4f476cb77de0efb0fdf9607903
nat2119 <natnael256@gmail.com>
2022-12-26 03:56:45 -0800

    program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
