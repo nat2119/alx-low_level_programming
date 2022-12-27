99e2400584381703004528af612aad9a28720f01
nat2119 <natnael256@gmail.com>
2022-12-27 01:14:51 -0800

    prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+		if (n <= 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
