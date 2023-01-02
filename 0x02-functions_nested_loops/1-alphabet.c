9c545e6544c4b22e52a783b9f44b9970fad5837b
nat2119 <natnael256@gmail.com>
2023-01-02 03:30:03 -0800

    prints the alphabet, in lowercase, followed by a new line. haves it own proto type

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
