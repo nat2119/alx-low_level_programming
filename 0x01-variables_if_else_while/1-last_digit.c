fdb58df2ad438eeb0f84765ae9dfdfe2ae86e213
nat2119 <natnael256@gmail.com>
2023-03-31 06:51:27 -0700

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %d is %d and greater than 5\n", n, m);
 	else if (m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
-	else if (m < 6 && m != 0)
+	else if ((m < )6 && (m != 0))
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
