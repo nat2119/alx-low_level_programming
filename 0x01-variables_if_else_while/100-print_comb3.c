a049a22a8aea0a3ab36e6f7f33e85ab64aaee840
nat2119 <natnael256@gmail.com>
2023-04-01 08:03:10 -0700

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /*
  * main - Entery point
+ * Description: write a program that prints all possible
+ * different combinations of two digits
  * Return: Always 0 (success)
  */
 int main(void)
